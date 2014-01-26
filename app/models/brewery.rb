5a49a1ec5079e7f2f87fca6511fee866f6dd9743
Kalle Viiri <kviiri@cs.helsinki.fi>
2014-01-26 22:51:47 +0200

    13

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,6 @@
 class Brewery < ActiveRecord::Base
-  has_many :beers
+  has_many :beers, dependent: :destroy #dem colons
+  has_many :ratings, through: :beers   #such majick wow, very ruby
   def to_s
     name
   end
