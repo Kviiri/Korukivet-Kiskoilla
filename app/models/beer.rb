9a142764b335f31042e51ea27c841787e6854a45
Kalle Viiri <kviiri@cs.helsinki.fi>
2014-01-26 21:22:43 +0200

    kaljaaaa~

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = 0
+    ratings.each do |r|
+      sum += r.score
+    end
+    sum / ratings.count
+  end
 end
