e3fe99055cc16f8b514e26b7db71dc8ce6295a3e
Kalle Viiri <kviiri@cs.helsinki.fi>
2014-02-02 23:43:00 +0200

    rating-user dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 4 }
   validate :password_format_is_correct
 
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
   has_many :memberships
   has_many :beer_clubs, through: :memberships
