0c57cacdf7d6cec617eedd21e4569cee92f7ec33
Kalle Viiri <kviiri@cs.helsinki.fi>
2014-02-02 17:34:11 +0200

    prev rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Ratebeer::Application.routes.draw do
+  resources :users
+
   resources :beers
   resources :breweries
 
   root 'breweries#index'
+  
 #  get 'kaikki_bisset', to: 'beers#index'
 #  get 'ratings', to: 'ratings#index'
 #  get 'ratings/new', to:'ratings#new'
 #  post 'ratings', to: 'ratings#create'
   resources :ratings, only: [:index, :new, :create, :destroy]
+  get 'signup', to:users#new
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
