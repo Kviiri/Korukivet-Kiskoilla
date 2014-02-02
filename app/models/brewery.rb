32f588e472f11b0b0901c0bb44311d43b6e24059
Kalle Viiri <kviiri@cs.helsinki.fi>
2014-02-02 21:03:55 +0200

    validointeja

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,10 @@
 class Brewery < ActiveRecord::Base
   include RatingAverage
+  validates :name, presence: true
+  validates :year, numericality: { greater_than_or_equal_to: 1042,
+    less_than_or_equal_to: 2014,
+    only_integer: true }
+
   has_many :beers, dependent: :destroy #dem colons
   has_many :ratings, through: :beers   #such majick wow, very ruby
   def to_s
