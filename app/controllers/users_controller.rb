5490be72bed268d0a2cc034b62f9a5d81aedf899
Kalle Viiri <kviiri@cs.helsinki.fi>
2014-02-03 16:34:00 +0200

    t12 tehty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,11 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy if @user == current_user
+    if @user == current_user
+      @user.destroy
+      session[:user_id] = nil
+    end
+    
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
